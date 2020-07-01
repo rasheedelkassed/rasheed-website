c2135eca843b33b7545cac4cd953e2c16a1ea80e
rasheedelkassed <relkassed@gmail.com>
2020-07-01 11:14:08 -0700

    Add stub of the SocialsNavbar component

---
+++ src/components/SocialsNavbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+class SocialsNavbar extends Component {
+    render() {
+      
+    }
+  }
+
+export default SocialsNavbar;
