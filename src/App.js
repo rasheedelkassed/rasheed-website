ca2619f726c5e68cb60552299c696267037760e2
rasheedelkassed <relkassed@gmail.com>
2020-07-01 12:39:40 -0700

    Add bottom nav bar for social media links

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MainNavbar from './components/MainNavbar'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SocialsNavbar from './components/SocialsNavbar';
 
 function App() {
   return (
     <div className="App">
       <MainNavbar></MainNavbar>
+      <SocialsNavbar></SocialsNavbar>
     </div>
   );
 }
--- src/components/SocialsNavbar.js
+++ src/components/SocialsNavbar.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import { GitHub, Linkedin } from 'react-feather';
 
 class SocialsNavbar extends Component {
     render() {
-      
+        return(
+            <Navbar bg="light" expand="lg" fixed="bottom">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link href="#github"><GitHub></GitHub></Nav.Link>
+                  <Nav.Link href="#linkedin"><Linkedin></Linkedin></Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          )
+
     }
   }
 
