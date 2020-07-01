e6367ac2288c421df87ea9b856f8afb0ae108644
rasheedelkassed <relkassed@gmail.com>
2020-07-01 11:31:34 -0700

    Add funtionless Main Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import MainNavbar from './components/MainNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainNavbar></MainNavbar>
     </div>
   );
 }
--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 
 class MainNavbar extends Component {
     render() {
+      return(
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Rasheed El Kassed</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      )
       
     }
   }
