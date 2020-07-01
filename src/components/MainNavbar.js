f85727605d9b8e201776acc3c8ab4bfb6365d1e8
rasheedelkassed <relkassed@gmail.com>
2020-07-01 11:05:27 -0700

    Add stub of the website\'s main navbar

---
+++ src/components/MainNavbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+class Navbar extends Component {
+    render() {
+      
+    }
+  }
+
+export default Navbar;
